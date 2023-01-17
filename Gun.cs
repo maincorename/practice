93cadcba2c9b33042e5cdb630958a8b20c69f8a2
maincorename <turbocatrom@gmail.com>
2023-01-17 16:59:10 +0700

    Создание базы - Класс с оружием - Класс с юнитами - Проверка работоспособности

---
+++ Gun.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePracticeApp
+{
+    /// <summary>
+    /// Класс оружия.
+    /// </summary>
+    internal class Gun
+    {
+        int AmmoCount;
+        public Gun()
+        {
+            Reload();
+        }
+        public void Reload()
+        {
+            AmmoCount = 10;
+        }
+        public int CheckAmmo()
+        {
+            return AmmoCount;
+        }
+        public void Fire()
+        {
+            AmmoCount -= 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsolePracticeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя солдата: ");
+            var nameSoldier = Console.ReadLine();
+            if(nameSoldier is "")
+            {
+                Console.WriteLine("Имя не может быть пустым.");
+            }
+
+            var gun1 = new Gun();
+            var gun2 = new Gun();
+
+            var warrior2 = new Soldier(gun2, nameSoldier);
+            var warrior = new Soldier(gun1);
+           
+            warrior.Fire();
+            var ammo = gun1.CheckAmmo();
+
+            while(ammo > 0)
+            {
+                warrior.Fire();
+                Console.WriteLine(ammo);
+                ammo = gun1.CheckAmmo();
+            }
+
+            gun1.Reload();
+            Console.WriteLine("RELOADING! READY TO FIGHT!");
+
+            var random = new Random();
+            var value = random.Next(1, 10);
+            Console.WriteLine(value);
+            if (value > 0)
+                Console.WriteLine(warrior2._name.ToString() + " is DEAD.");
+        }
+    }
+}
